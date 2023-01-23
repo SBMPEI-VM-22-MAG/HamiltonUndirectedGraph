417561ffd60b1330371b87ed43121aee0b618a74
kinwend17 <st.andrey17@yandex.ru>
2023-01-23 17:29:20 +0300

    some

---
+++ Models/GraphModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamiltonUndirectedGraph.Models;
+
+public class GraphModel
+{
+    public int VertexCount { get; private set; }
+    public List<int[]> Edges { get; private set; }
+
+    public GraphModel(int vertexCount)
+    {
+        if (vertexCount >= 2)
+        {
+            vertexCount = vertexCount;
+            Edges = new List<int[]>();
+        }
+    }
+}
